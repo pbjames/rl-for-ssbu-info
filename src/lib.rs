4f7126fa163c560f1fc4688d67ded3ac6c38e517
James <jameskowal10@gmail.com>
2026-07-14 20:40:06 +0100

    Rename folders and update code

--- src/lib.rs
+++ src/lib.rs
@@ -12,9 +12,9 @@
     clippy::borrow_interior_mutable_const
 )]
 
-mod EDIT;
+mod hooks;
 
 #[skyline::main(name = "smashline_test")]
 pub fn main() {
-    EDIT::install();
+    hooks::install();
 }
