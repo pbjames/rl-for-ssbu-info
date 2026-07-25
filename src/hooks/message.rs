55c1b3dbe8fa53e098cd789b522ebb94ffd0fe7b
James <jameskowal10@gmail.com>
2026-07-25 13:17:33 +0100

    Implement messagepack and include more inputs

--- src/hooks/message.rs
+++ src/hooks/message.rs
@@ -1,22 +1,204 @@
-use std::{io::Write, net::TcpStream};
-
 use serde::{Deserialize, Serialize};
+use smash::lib::lua_const::{
+    *
+};
+use std::{io::Write, net::TcpStream, ops::ShrAssign, sync::LazyLock};
+use rmp_serde::Serializer;
+
+use super::STREAM;
 
 #[derive(Serialize, Deserialize, Debug, Default)]
 pub struct Message {
     pub stage: usize,
-    pub cpu_x: f32,
-    pub cpu_y: f32,
-    pub cpu_damage: f32,
-    pub opp_x: f32,
-    pub opp_y: f32,
-    pub opp_damage: f32,
+    pub cpu: FighterInfo,
+    pub opp: FighterInfo,
+}
+
+#[derive(Serialize, Deserialize, Debug, Default)]
+pub struct FighterInfo {
+    pub location: Vector3fInfo,
+    pub damage: f32,
+    pub is_shield: bool,
+    pub shield_strength: f32,
+    pub attack: AttackInfo,
+    pub grounded_ke: Vector3fInfo,
+    pub situation: SituationKindInfo,
+    pub status: StatusKindInfo,
+}
+
+#[derive(Serialize, Deserialize, Debug, Default)]
+pub enum SituationKindInfo {
+    #[default]
+    None,
+    Air,
+    Odd,
+    Term,
+    Cliff,
+    Ground,
+    Ladder,
+    Outfield,
+    Restraint,
+    Water,
+}
+
+impl From<i32> for SituationKindInfo {
+    fn from(x: i32) -> Self {
+        let none: i32 = SITUATION_KIND_NONE.into();
+        let air: i32 = SITUATION_KIND_AIR.into();
+        let odd: i32 = SITUATION_KIND_ODD.into();
+        let cliff: i32 = SITUATION_KIND_CLIFF.into();
+        let ground: i32 = SITUATION_KIND_GROUND.into();
+        let ladder: i32 = SITUATION_KIND_LADDER.into();
+        let outfield: i32 = SITUATION_KIND_OUTFIELD.into();
+        let restraint: i32 = SITUATION_KIND_RESTRAINT.into();
+        let water: i32 = SITUATION_KIND_WATER.into();
+        if x == none {
+            Self::None
+        } else if x == air {
+            Self::Air
+        } else if x == odd {
+            Self::Odd
+        } else if x == cliff {
+            Self::Cliff
+        } else if x == ground {
+            Self::Ground
+        } else if x == ladder {
+            Self::Ladder
+        } else if x == outfield {
+            Self::Outfield
+        } else if x == restraint {
+            Self::Restraint
+        } else if x == water {
+            Self::Water
+        } else {
+            Self::None
+        }
+    }
+}
+
+#[derive(Serialize, Deserialize, Debug, Default)]
+pub enum StatusKindInfo {
+    #[default]
+    Unmapped,
+    Ice,
+    Run,
+    Bury,
+    Fall,
+    Dash,
+    Jump,
+    Lose,
+    Walk,
+    Grab,
+    Sleep,
+    CliffWait,
+    CliffCatch,
+    CliffClimb,
+    CliffJump1,
+    CliffJump2,
+    CliffJump3,
+    CliffAttack,
+    CliffEscape,
+    EscapeAir,
+    Escape,
+    EscapeB,
+    EscapeF,
+    Dead,
+}
+
+impl From<i32> for StatusKindInfo {
+    fn from(x: i32) -> Self {
+        let ice: i32 = FIGHTER_STATUS_KIND_ICE.into();
+        let run: i32 = FIGHTER_STATUS_KIND_RUN.into();
+        let bury: i32 = FIGHTER_STATUS_KIND_BURY.into();
+        let dash: i32 = FIGHTER_STATUS_KIND_DASH.into();
+        let dead: i32 = FIGHTER_STATUS_KIND_DEAD.into();
+        let jump: i32 = FIGHTER_STATUS_KIND_JUMP.into();
+        let walk: i32 = FIGHTER_STATUS_KIND_WALK.into();
+        let catch: i32 = FIGHTER_STATUS_KIND_CATCH.into();
+        let sleep: i32 = FIGHTER_STATUS_KIND_SLEEP.into();
+        let cliff_wait: i32 = FIGHTER_STATUS_KIND_CLIFF_WAIT.into();
+        let cliff_attack: i32 = FIGHTER_STATUS_KIND_CLIFF_ATTACK.into();
+        let cliff_catch: i32 = FIGHTER_STATUS_KIND_CLIFF_CATCH.into();
+        let cliff_climb: i32 = FIGHTER_STATUS_KIND_CLIFF_CLIMB.into();
+        let cliff_jump1: i32 = FIGHTER_STATUS_KIND_CLIFF_JUMP1.into();
+        let cliff_jump2: i32 = FIGHTER_STATUS_KIND_CLIFF_JUMP2.into();
+        let cliff_jump3: i32 = FIGHTER_STATUS_KIND_CLIFF_JUMP3.into();
+        let cliff_escape: i32 = FIGHTER_STATUS_KIND_CLIFF_ESCAPE.into();
+        let escape_air: i32 = FIGHTER_STATUS_KIND_ESCAPE_AIR.into();
+        let escape: i32 = FIGHTER_STATUS_KIND_ESCAPE.into();
+        let escape_b: i32 = FIGHTER_STATUS_KIND_ESCAPE_B.into();
+        let escape_f: i32 = FIGHTER_STATUS_KIND_ESCAPE_F.into();
+        if x == ice {
+            Self::Ice
+        } else if x == run {
+            Self::Run
+        } else if x == bury {
+            Self::Bury
+        } else if x == dash {
+            Self::Dash
+        } else if x == dead {
+            Self::Dead
+        } else if x == jump {
+            Self::Jump
+        } else if x == walk {
+            Self::Walk
+        } else if x == catch {
+            Self::Grab
+        } else if x == sleep {
+            Self::Sleep
+        } else if x == cliff_wait {
+            Self::CliffWait
+        } else if x == cliff_attack {
+            Self::CliffAttack
+        } else if x == cliff_catch {
+            Self::CliffCatch
+        } else if x == cliff_climb {
+            Self::CliffClimb
+        } else if x == cliff_jump1 {
+            Self::CliffJump1
+        } else if x == cliff_jump2 {
+            Self::CliffJump2
+        } else if x == cliff_jump3 {
+            Self::CliffJump3
+        } else if x == cliff_escape {
+            Self::CliffEscape
+        } else if x == escape_air {
+            Self::EscapeAir
+        } else if x == escape {
+            Self::Escape
+        } else if x == escape_b {
+            Self::EscapeB
+        } else if x == escape_f {
+            Self::EscapeF
+        } else {
+            Self::Unmapped
+        }
+    }
+}
+
+#[derive(Serialize, Deserialize, Debug, Default)]
+pub struct AttackInfo {
+    pub is_attack: bool,
+    pub is_landed: bool,
+    pub is_grab: bool,
+    pub power: f32,
+    pub knockback_growth: i32,
+    pub fixed_knockback: i32,
+    pub bonus_knockback: i32,
+    pub bb1: Vector3fInfo,
+    pub bb2: Vector3fInfo,
+}
+
+#[derive(Serialize, Deserialize, Debug, Default)]
+pub struct Vector3fInfo {
+    pub x: f32,
+    pub y: f32,
+    pub z: f32,
 }
 
 impl Message {
     pub fn send_state(&mut self, stream: &mut TcpStream) -> std::io::Result<()> {
-        let bytes = serde_json::to_vec(self).unwrap();
-        stream.write_all(&bytes)?;
+	self.serialize(&mut Serializer::new(stream)).unwrap();
         Ok(())
     }
 }
--- src/hooks/mod.rs
+++ src/hooks/mod.rs
@@ -1,13 +1,13 @@
 mod constants;
 mod message;
-use constants::{HOST, OPP_INDEX, OUR_INDEX};
+use constants::{CPU_INDEX, HOST, OPP_INDEX};
 use std::{
     io::Write,
     net::{self, TcpStream},
     sync::LazyLock,
 };
 
-use crate::hooks::message::Message;
+use crate::hooks::message::{Message, SituationKindInfo, StatusKindInfo};
 
 use {
     smash::{
@@ -21,7 +21,11 @@ use {
     smashline::{Priority::*, *},
 };
 
-static mut STREAM: LazyLock<TcpStream> = LazyLock::new(|| TcpStream::connect(HOST).unwrap());
+static mut STREAM: LazyLock<TcpStream> = LazyLock::new(|| {
+    let stream = TcpStream::connect(HOST).unwrap();
+    stream.set_nodelay(true);
+    stream
+});
 static mut MESSAGE_BUF: LazyLock<Message> = LazyLock::new(|| Message::default());
 
 #[skyline::hook(replace = StatusModule::situation_kind)]
@@ -31,32 +35,56 @@ unsafe fn situation_kind_replace(
     let player_slot =
         WorkModule::get_int(module_accessor, *FIGHTER_INSTANCE_WORK_ID_INT_ENTRY_ID) as usize;
     // BUG: Completely inaccurate opponent data Pokemon Stadium stage
-    if player_slot == OPP_INDEX {
-        MESSAGE_BUF.opp_x = PostureModule::pos_x(module_accessor);
-        MESSAGE_BUF.opp_y = PostureModule::pos_y(module_accessor);
-        MESSAGE_BUF.opp_damage = DamageModule::damage(module_accessor, 0);
-        // Opponent shield
-        // Opponent attack
-        // Opponent movement (kinetics)
-        // Opponent state
-        // Opponent Character
+    let target = match player_slot {
+        0 => &mut MESSAGE_BUF.opp,
+        1 => &mut MESSAGE_BUF.cpu,
+        _ => &mut MESSAGE_BUF.opp,
+    };
+    MESSAGE_BUF.stage = stage::get_stage_id() as usize;
+    let vel_3f =
+        KineticModule::get_sum_speed3f(module_accessor, *KINETIC_ENERGY_RESERVE_ATTRIBUTE_MAIN);
+    let is_attack = AttackModule::is_infliction(module_accessor, *COLLISION_KIND_MASK_ALL);
+    target.location.x = PostureModule::pos_x(module_accessor);
+    target.location.y = PostureModule::pos_y(module_accessor);
+    target.location.z = PostureModule::pos_z(module_accessor);
+    target.damage = DamageModule::damage(module_accessor, 0);
+    target.is_shield = ShieldModule::is_shield(module_accessor, 0, 0);
+    target.shield_strength = ShieldModule::get_attack_limit(module_accessor, 0);
+    // target.situation = SituationKindInfo::from(StatusModule::situation_kind(module_accessor));
+    // target.status = StatusKindInfo::from(StatusModule::status_kind(module_accessor));
+    target.attack.is_attack = is_attack;
+    if target.attack.is_attack {
+        let is_attack_landed =
+            AttackModule::is_infliction_status(module_accessor, *COLLISION_KIND_MASK_ALL);
+        let attack_data = *AttackModule::attack_data(module_accessor, 0, false);
+        target.attack.is_landed = is_attack_landed;
+        target.attack.power = attack_data.power;
+        target.attack.knockback_growth = attack_data.r_eff;
+        target.attack.fixed_knockback = attack_data.r_fix;
+        target.attack.bonus_knockback = attack_data.r_add;
+        target.attack.bb1.x = attack_data.x;
+        target.attack.bb1.y = attack_data.y;
+        target.attack.bb1.z = attack_data.z;
+        target.attack.bb2.x = attack_data.x2;
+        target.attack.bb2.y = attack_data.y2;
+        target.attack.bb2.z = attack_data.z2;
     }
-    if player_slot == OUR_INDEX {
-        // INFO: n -> 310 signal END 310 -> n signal START
-        MESSAGE_BUF.stage = stage::get_stage_id() as usize;
-        MESSAGE_BUF.cpu_x = PostureModule::pos_x(module_accessor);
-        MESSAGE_BUF.cpu_y = PostureModule::pos_y(module_accessor);
-        MESSAGE_BUF.cpu_damage = DamageModule::damage(module_accessor, 0);
-        MESSAGE_BUF
-            .send_state(&mut STREAM)
-            .map_err(|err| println!("{}", err));
-    }
+    target.grounded_ke.x = vel_3f.x;
+    target.grounded_ke.y = vel_3f.y;
+    target.grounded_ke.z = vel_3f.z;
+    MESSAGE_BUF
+        .send_state(&mut STREAM)
+        .map_err(|err| println!("{}", err))
+        .unwrap();
+    STREAM.write_all(b"END");
     original!()(module_accessor)
 }
+
+// unsafe extern "C" fn agent_init(fighter: &mut L2CFighterCommon) {}
 
 pub fn install() {
     skyline::install_hooks!(situation_kind_replace);
     //Agent::new("fighter")
-    //    .on_start(agent_init)
-    //    .on_end(agent_deinit);
+    //.on_start(agent_init)
+    //.on_end(agent_deinit);
 }
